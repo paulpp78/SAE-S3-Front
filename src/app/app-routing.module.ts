ffef99de506d3233e83cb252ed6968550b9c0e29
paulpp78 <paulperigault@wevii.net>
2023-10-17 01:36:12 +0200

    Ajout des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommunauteComponent } from './communaute/communaute.component';
+import { EvenementComponent } from './evenement/evenement.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'communaute', component: CommunauteComponent },
+  { path: 'evenement', component: EvenementComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateBackgroundClasses(event.url);
+      }
+    });
+  }
+
+  updateBackgroundClasses(url: string) {
+    const header = document.querySelector('.header') as HTMLElement;
+
+    // Supprimez les classes de fond existantes
+    header.classList.remove(
+      'home-background',
+      'communaute-background',
+      'evenement-background',
+      'contact-background'
+    );
+
+    // Ajoutez la classe de fond correspondante en fonction de la route
+    if (url === '/home') {
+      header.classList.add('home-background');
+    } else if (url === '/communaute') {
+      header.classList.add('communaute-background');
+    } else if (url === '/evenement') {
+      header.classList.add('evenement-background');
+    } else if (url === '/contact') {
+      header.classList.add('contact-background');
+    }
+  }
+
   isMenuClosed: boolean = true;
 
   toggleMenu() {
     this.isMenuClosed = !this.isMenuClosed;
   }
   weatherInfo: string = '';
-
+  city: string = 'Denain';
   ngOnInit() {
     const apiKey = 'bdb616a0479919c67aecbc38787f1d02';
     const city = 'Denain,fr';
