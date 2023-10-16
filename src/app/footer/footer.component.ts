3809a63f0fe82ada73e8098bdf9e9e0125ea813f
paulpp78 <paulperigault@wevii.net>
2023-10-16 03:41:01 +0200

    Creation projet Angular

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: [
+    './footer.component.css',
+    '../../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+  ],
+})
+export class FooterComponent {
+  address: string = 'Av, Michel Rondet.';
+  city: string = '59135 Wallers';
+  phoneNumber: string = '03 27 09 05 05';
+
+  onContactButtonClicked() {
+    console.log('Bouton de contact cliqué !');
+  }
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: [
+    './header.component.css',
+    '../../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+  ],
+})
+export class HeaderComponent implements OnInit {
+  weatherInfo: string = '';
+
+  ngOnInit() {
+    const apiKey = 'bdb616a0479919c67aecbc38787f1d02';
+    const city = 'Denain,fr';
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          console.error("La requête vers l'API a échoué");
+          throw new Error("La requête vers l'API a échoué");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data && data.main && data.main.temp) {
+          const temperature = Math.round(data.main.temp - 273.15);
+
+          this.weatherInfo = `Température : ${temperature}°C`;
+          console.log('Données météo récupérées avec succès :', data);
+        } else {
+          console.error(
+            "Données météo non disponibles dans la réponse de l'API"
+          );
+          console.error("Réponse de l'API :", data);
+        }
+      })
+      .catch((error) => {
+        console.error(
+          'Erreur lors de la récupération des données météo',
+          error
+        );
+      });
+  }
+}
