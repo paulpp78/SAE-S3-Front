0b19dc784853f5dd6110fb67400f95ea26297ce6
paulpp78 <paulperigault@wevii.net>
2023-10-17 16:32:58 +0200

    Ajout du server pour la prod

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
