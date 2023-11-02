f959979f54cfd1ad8709ac2c41f89afdf062ef0d
paulpp78 <paulperigault@wevii.net>
2023-11-03 00:31:14 +0100

    Ajout de la mise en prod

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Importation des modules requis
 const express = require("express"); // Express est un framework web pour Node.js
-const app = express(); // Créez une instance d'Express
+
+const app = express();
 const path = require("path"); // Module pour gérer les chemins de fichiers
 const cors = require("cors"); // Middleware pour gérer les requêtes CORS
 const fs = require("fs"); // Module pour la gestion des fichiers
@@ -8,18 +9,19 @@ const configFile = fs.readFileSync("config.json", "utf8"); // Lire le fichier de
 const config = JSON.parse(configFile); // Analyser le fichier de configuration en JSON
 
 // Configuration du middleware
-app.use(cors({ origin: config.corsConf })); // Active la gestion des requêtes CORS avec la configuration du fichier
 app.use(express.json()); // Active le parsing automatique des données JSON dans les requêtes
+
+app.use(cors({ origin: config.corsConf })); // Active la gestion des requêtes CORS
 
 // Configuration des fichiers statiques
 app.use(express.static(path.join(__dirname, config.staticDirectory))); // Définit le répertoire statique
 
-// Route par défaut (pour toutes les autres routes)
+// Configuration des routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, config.staticDirectory, config.firstPage)); // Envoie la page d'accueil statique
 });
 
 // Démarrage du serveur
 app.listen(config.port, () => {
-  console.log(`Serveur en cours d'exécution sur le port ${config.port}`); // Affiche un message lors du démarrage du serveur
+  console.log(`Serveur en cours d'exécution sur le port ${config.port}`);
 });
